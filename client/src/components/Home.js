a5bd607732ddb4d505b0890fbff65480a9c0bcaf
momenashrafff <momenashraf742@gmail.com>
2024-05-17 20:55:34 +0300

    finished Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import Products from "./Products";
 import Orders from "./Orders";
-
+import './Home.css';
 const Home = () => {
     return (
         <div className="wrapper2">
